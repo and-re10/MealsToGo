2ab16f0c5ef7218bbce0058b3506a5f58ef2c399
and-re10 <andreandre_1234@hotmail.com>
2022-05-26 18:16:31 +0200

    Status bar spacing in IOS and Android

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+
+  // const isAndroid = Platform.OS === "android";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <SafeAreaView style={{flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0}}> */}
+      <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight}}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.container}>
+          <Text>Our React Native blank canvas</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  search: {
+    // flex: 0.1,
+    // backgroundColor: "green",
+    // justifyContent: "center",
+    // paddingLeft: 20
+    padding: 16,
+    backgroundColor: "green",
   },
+  container: { 
+    // flex: 0.9,
+    // backgroundColor: "blue",
+    // paddingLeft: 20,
+    // paddingTop: 20
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16
+  }
 });
