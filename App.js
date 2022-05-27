e2a4fd6d1810866cef9120b3ef203c896975dad9
and-re10 <andreandre_1234@hotmail.com>
2022-05-27 10:33:28 +0200

    install eslint, prettier and add searchbar

--- App.js
+++ App.js
@@ -1,41 +1,54 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import React from "react";
 
 export default function App() {
-
   // const isAndroid = Platform.OS === "android";
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <>
       {/* <SafeAreaView style={{flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0}}> */}
-      <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight}}>
+      <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>Search</Text>
+          <Searchbar
+            style={styles.search}
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
         </View>
-        <View style={styles.container}>
+        <View style={styles.list}>
           <Text>Our React Native blank canvas</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style="auto"/>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
   search: {
     // flex: 0.1,
     // backgroundColor: "green",
     // justifyContent: "center",
     // paddingLeft: 20
     padding: 16,
-    backgroundColor: "green",
+    // backgroundColor: "green",
   },
-  container: { 
+  list: {
     // flex: 0.9,
     // backgroundColor: "blue",
     // paddingLeft: 20,
     // paddingTop: 20
     flex: 1,
     backgroundColor: "blue",
-    padding: 16
-  }
+    padding: 16,
+  },
 });
