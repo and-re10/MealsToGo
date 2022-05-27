83b0f1fdbf77918eb930506b50d3c524a29e6809
and-re10 <andreandre_1234@hotmail.com>
2022-05-27 15:24:35 +0200

    install styled-components and creating first component

--- App.js
+++ App.js
@@ -1,54 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
-import { Searchbar } from "react-native-paper";
 import React from "react";
+// Screens
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
 
 export default function App() {
-  // const isAndroid = Platform.OS === "android";
-  const [searchQuery, setSearchQuery] = React.useState("");
-
-  const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <>
-      {/* <SafeAreaView style={{flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0}}> */}
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            style={styles.search}
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>Our React Native blank canvas</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    // flex: 0.1,
-    // backgroundColor: "green",
-    // justifyContent: "center",
-    // paddingLeft: 20
-    padding: 16,
-    // backgroundColor: "green",
-  },
-  list: {
-    // flex: 0.9,
-    // backgroundColor: "blue",
-    // paddingLeft: 20,
-    // paddingTop: 20
-    flex: 1,
-    backgroundColor: "blue",
-    padding: 16,
-  },
-});
---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { SafeAreaView, View, StyleSheet, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantsScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar
+          style={styles.search}
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfoCard />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
+  },
+});
