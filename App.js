0bc9663fddb83b441a43a9f4b327846a730f51c8
and-re10 <andreandre_1234@hotmail.com>
2022-05-28 12:25:47 +0200

    Add Google Fonts, SVG and Spacing Components

--- App.js
+++ App.js
@@ -2,12 +2,34 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 // Screens
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
+//Theme
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastrcture/theme/index";
+//Fonts
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
-  return (
-    <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
-    </>
-  );
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  } else {
+    return (
+      <>
+        <ThemeProvider theme={theme}>
+          <RestaurantsScreen />
+        </ThemeProvider>
+        <ExpoStatusBar style="auto" />
+      </>
+    );
+  }
 }
